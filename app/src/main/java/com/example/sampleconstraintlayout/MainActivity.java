a4b9ce1f063470049967305f75ba321111f61ac8
annisaysfn <annisa.yasfa25@gmail.com>
2021-04-21 15:52:03 +0800

    Activity2C

--- app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
+++ app/src/main/java/com/example/sampleconstraintlayout/MainActivity.java
@@ -47,79 +47,29 @@ public class MainActivity extends AppCompatActivity {
                 //Menyimpan input user di edittext password ke dalam variabel password
                 password = edpassword.getText().toString();
 
-                //Mengeset email yang benar
-                String email = "admin@mail.com";
-
-                //Mengeset password yang benar
-                String pass = "123";
+                String email = "annisayasfa@mail.com";
+                String pass = "222";
 
                 //Mengecek apakah edittext email dan password terdapat isi atau tidak
-                if (nama.isEmpty() || password.isEmpty()) {
-                    //Membuat variabel toast dan menampilkan pesan "editText tidak boleh kosong"
-                    Toast t = Toast.makeText(getApplicationContext(), "Email dan Password wajib diisi!", Toast.LENGTH_LONG);
-
-                    //Menampilkan toast
+                if (nama.equals(email) && password.equals(pass))
+                {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Login Berhasil", Toast.LENGTH_LONG);
+                    t.show();
+                } else if (!nama.equals(email) && password.equals(pass)) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email Salah", Toast.LENGTH_LONG);
+                    t.show();
+                } else if (nama.equals(email) && !password.equals(pass)) {
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Password Salah", Toast.LENGTH_LONG);
                     t.show();
                 } else {
-
-                    //Mengecek apakah isi dari email dan password sudah sama dengan email dan
-                    //password yang sudah di set
-                    if (nama.equals(email) && password.equals(pass)) {
-                        //Membuat variabel toast dan menampilkan pesan "Login sukses"
-                        Toast t = Toast.makeText(getApplicationContext(),
-                                "Login Sukses", Toast.LENGTH_LONG);
-                        //Menampilkan toast
-                        t.show();
-
-                        //Membuat objek bundle
-                        Bundle b = new Bundle();
-
-                        //Memasukkan value dari variabel nama dengan kunci "a"
-                        //dan dimasukan ke dalam bundle
-                        b.putString("a", nama.trim());
-
-                        //Memasukkan value dari variabel password dengan kunci "b"
-                        //dan dimasukkan ke dalam bundle
-                        b.putString("b", password.trim());
-
-                        //Membuat objek intent berpindah activity dari mainactivity ke ActivityHasil
-                        Intent i = new Intent(getApplicationContext(), ActivityHasil.class);
-
-                        //Memasukkan bundle ke dalam intent untuk dikirimkan ke ActivityHasil
-                        i.putExtras(b);
-
-                        //Berpindah ke ActivityHasil
-                        startActivity(i);
-                    } else {
-                        //Membuat variabel toast dan membuat toast dan menampilkan pesan "Login gagal"
-
-                        Toast t = Toast.makeText(getApplicationContext(),
-                                "Login Gagal", Toast.LENGTH_LONG);
-
-                        //Menampilkan toast
-                        t.show();
-                    }
+                    Toast t = Toast.makeText(getApplicationContext(),
+                            "Email dan Password Salah", Toast.LENGTH_LONG);
+                    t.show();
                 }
             }
         });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        //Method untuk menampilkan menu
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        //Membuat kondisi jika yang dipilih adalah id mnDaftar
-        if (item.getItemId() == R.id.mnDaftar)
-        {
-            //Method untuk memanggil activity "ActivityDaftar"
-            Intent i = new Intent(getApplicationContext(), ActivityDaftar.class);
-            startActivity(i);
-        }
-        return super.onOptionsItemSelected(item);
     }
 }
